629d66e7018de6fdb99d3090a7bc4d7e0b0568aa
XanthosWare <70963740+XanthosWare@users.noreply.github.com>
2020-10-01 21:39:19 +0600

    Version 0.1.0
    Prototype contains player, plant-maker, dungeon, enemy, companion. NPC do not move.

---
+++ CHLOROPHYL DEVTOBER 0.1.0/Environments/Dungeon Crawler/Dungeon.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Teleporter_Area2D_area_entered(area):
+	var debugtext = get_node("Visual Text Debug")
+	debugtext.set_text("teleported")
+	get_tree().change_scene("res://Environments/Green Home/Home/GreenHome.tscn")
+
+func _on_Area2D_area_entered(area):
+	var debugtext = get_node("Visual Text Debug")
+	debugtext.set_text("Enemy died")
